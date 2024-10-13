72c756ff560aa748a5a29b8f31057569e782cf8f
Chronos69420 <zhuyunsong02097007@gmail.com>
2024-10-13 12:46:18 +0800

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          <span>
-            <strong>Holy guacamole!</strong> You should check in on some of
-            those fields below.
-          </span>
-        </Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        Show Alert
-      </Button>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
